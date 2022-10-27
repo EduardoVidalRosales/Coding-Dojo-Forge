a5e69e2dc3c7b1579a8717d561fa4724a4334813
EduardoVidalRosales <116766934+EduardoVidalRosales@users.noreply.github.com>
2022-10-26 20:51:24 -0500

    Add files via upload

---
+++ CODING-DOJO/LastName_FirstName_JS_CrearVariables.js
@@ -0,0 +1,4 @@
+var altura = 180;
+var edad = 25;
+// estos dos son los que definen y ayudan a determinar las variables para poder entrar a la montaña ruasa
+// se debe añadir un if para determinar si pueden o no subirse
---
+++ CODING-DOJO/feria-codingdojo.js
@@ -0,0 +1,20 @@
+var Laltura = 52 //Limite de altura definido = 1.60 (1 metro 60 centimetros)
+var Ledad = 16     //Lmite de edad definido = 16 años
+var altura = Math.floor(Math.random() * 100); // altura del visitante
+var edad = Math.floor(Math.random() * 50); // edad del visitante
+var check = 0
+console.log(altura, edad);
+if (Laltura > altura) {   // si el limite de altura es mayor al del visitante arroja una negativa 
+    console.log('no cumples con el limite de altura, Lo siento, chico. Tal vez el próximo año');
+    check =+ 1 // aqui se agrega un 1 al check para la comprobacion final
+}    
+if (Ledad > edad) { // si el limite de edad es mayor al del visitante arroja una negativa 
+    console.log('no cumples con el limite de edad, Lo siento, chico. Tal vez el próximo año');
+    check =+ 1 // aqui se agrega un 1 al check para la comprobacion final
+}
+console.log(check)
+if (check == 0) { // si el check es igual a 0 arroja una positiva si es diferente a 0 arroja una negativa
+    console.log('"¡Súbete, chico!" '); // felicidades¡¡
+
+};
+ 
