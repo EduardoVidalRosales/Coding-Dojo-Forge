3932a516029c2d1d51d0c822f390028b999389ce
EduardoVidalRosales <116766934+EduardoVidalRosales@users.noreply.github.com>
2022-10-28 14:05:52 -0500

    Add files via upload

---
+++ CODING-DOJO/APELLIDO_NOMBRE_JSPredicción.js
@@ -0,0 +1,23 @@
+function myBirthYearFunc(){
+        console.log("Nací en" + 1980);
+    }
+//Nací en 1980
+
+
+
+
+function myBirthYearFunc(birthYearInput){
+        console.log("Nací en" + birthYearInput);
+    }
+//Nací en 1980
+
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 es: " + num1);
+    console.log("num2 es: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+// num1 es: 10, num2 es: 20
+//30
